72b1dd976528a1a9da9604c2949c9d07e8840c86
Shayan Kebriti <shayankebriti@gmail.com>
2023-12-25 22:42:18 +0330

    Add Rest API

--- elastic_data/connection.py
+++ elastic_data/connection.py
@@ -1,15 +1,15 @@
 from elasticsearch import Elasticsearch
 import pandas as pd
+from elasticsearch.helpers import bulk
 
 def connect():
     es = Elasticsearch(
         ['localhost'],
         port=9200
     )
-
     es.ping()
-
     return es
+
 
 def get_json_by_index(index_name):
     es = connect()
@@ -24,8 +24,27 @@ def get_json_by_index(index_name):
 
     return result
 
+
 def get_df_by_index(index_name):
     result = get_json_by_index(index_name)
     df = pd.DataFrame([hit['_source'] for hit in result['hits']['hits']])
     return df
 
+
+def generator(df, index_name):
+    for _, row in df.iterrows():
+        yield {
+            '_index': index_name,
+            '_type': "_doc",
+            '_source': row.to_json()
+        }
+
+
+def addBulk(csv, index_name):
+    es = connect()
+    bulk(es, generator(csv, index_name))
+
+
+def drop(index_name):
+    es = connect()
+    es.indices.delete(index=index_name)
--- rest_api.py
+++ rest_api.py
@@ -1,9 +1,15 @@
+# run it through:
+# uvicorn rest_api:app --reload
+
 import json
 import typing
 from fastapi import FastAPI
 from starlette.responses import Response
-
+import pandas as pd
 from elastic_data import connection
+from time import gmtime, strftime
+
+app = FastAPI()
 
 
 class PrettyJSONResponse(Response):
@@ -19,14 +25,54 @@ class PrettyJSONResponse(Response):
         ).encode("utf-8")
 
 
-app = FastAPI()
+async def addIndex(csv_file_path, index_name):
+    response = "successful!"
+    try:
+        csv = pd.read_csv(csv_file_path)
+        connection.addBulk(csv, index_name)
+    except:
+        response = "error!"
+    return {"response": response, "file_path": csv_file_path, "index_name": index_name}
 
 
-@app.get("/test")
+async def getIndex(index_name):
+    try:
+        return connection.get_json_by_index(index_name)
+    except:
+        return {"response": "error!", "index_name": index_name}
+
+
+async def dropIndex(index_name):
+    response = "successful!"
+    try:
+        connection.drop(index_name)
+    except:
+        response = "error!"
+    return {"response": response, "index_name": index_name}
+
+@app.get("", response_class=PrettyJSONResponse)
+@app.get("/", response_class=PrettyJSONResponse)
+@app.get("/test", response_class=PrettyJSONResponse)
 async def test():
-    return {"test": "successful!"}
+    return {"response": "Success! Server is up!", "Current Time":strftime("%Y-%m-%d %H:%M:%S", gmtime())}
 
-@app.get("/homes", response_class=PrettyJSONResponse)
-async def getHomes():
-    json = connection.get_json_by_index('housing_test')
-    return json
+
+@app.get("/add/{index_name}/from_csv/{csv_file_path:path}", response_class=PrettyJSONResponse)
+async def addIndexApi(index_name: str, csv_file_path: str):
+    return await addIndex(csv_file_path, index_name)
+
+
+@app.get("/{index_name}", response_class=PrettyJSONResponse)
+@app.get("/get/{index_name}/", response_class=PrettyJSONResponse)
+async def getIndexApi(index_name: str):
+    return await getIndex(index_name)
+
+
+
+@app.get("/drop/{index_name}", response_class=PrettyJSONResponse)
+async def dropIndexApiNotSure(index_name: str):
+    return {"Message":"are you sure? add /iamsure at the end of url if so."}
+
+@app.get("/drop/{index_name}/iamsure", response_class=PrettyJSONResponse)
+async def dropIndexApi(index_name: str):
+    return await dropIndex(index_name)
